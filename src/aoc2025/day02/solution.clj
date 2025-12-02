ee151264847a8944d58fc97d91f4eba109353eb7
Sheng Yunzhou <shengyunzhou@gmail.com>
2025-12-02 19:33:11 +0800

    Add day02 part 2 solution

--- src/aoc2025/day02/solution.clj
+++ src/aoc2025/day02/solution.clj
@@ -16,17 +16,59 @@
 (valid-id? 99)
 (valid-id? 1188511885)
 
-(defn parse-input [input]
+(defn solve-1 [input]
   (->> (str/split input #",")
        (map (fn [s]
               (str/split s #"-")))
        (map (fn [[start end]]
               (range (parse-long start)
-               (inc (parse-long end)))))
+                     (inc (parse-long end)))))
        (mapcat (fn [nums]
-              (filter valid-id? nums)))
+                 (filter valid-id? nums)))
        (reduce +)))
 
-(parse-input sample-input)
+(solve-1 sample-input)
 
-(parse-input (slurp "resources/day02/input.txt"))
+(solve-1 (slurp "resources/day02/input.txt"))
+
+(partition 3 "121212")
+
+(defn valid-id-2? [id-num]
+  (let [id (str id-num)]
+    (loop [partition-count 1
+           found-repeat false]
+      (cond
+        found-repeat true
+        (> partition-count (count id)) false
+        (> (mod (count id) partition-count) 0) (recur (inc partition-count) found-repeat)
+        :else (let [parts (partition partition-count id)
+                    first-part (first parts)
+                    all-same? (every? #(= first-part %) parts)]
+                (recur (inc partition-count)
+                       (and (> (count parts) 1) all-same?)))))))
+
+(valid-id-2? 123123)
+(valid-id-2? 1212)
+(valid-id-2? 12121212)
+;; false
+(valid-id-2? 13)
+(valid-id-2? 2121212121)
+;; false
+(valid-id-2? 2121212122)
+(valid-id-2? 1188511885)
+(valid-id-2? 11)
+
+
+(defn solve-2 [input]
+  (->> (str/split input #",")
+       (map (fn [s]
+              (str/split s #"-")))
+       (map (fn [[start end]]
+              (range (parse-long start)
+                     (inc (parse-long end)))))
+       (mapcat (fn [nums]
+                 (filter valid-id-2? nums)))
+       (reduce +)))
+
+(solve-2 sample-input)
+(solve-2 (slurp "resources/day02/input.txt"))
