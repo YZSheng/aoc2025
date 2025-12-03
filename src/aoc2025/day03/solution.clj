f0db8eba50ecded8baa94fdd8b550a3a0df9ad46
Sheng Yunzhou <shengyunzhou@gmail.com>
2025-12-03 20:26:16 +0800

    Add day03 part 2 solution

--- src/aoc2025/day03/solution.clj
+++ src/aoc2025/day03/solution.clj
@@ -1,4 +1,4 @@
-(ns aoc2025.day03.solution 
+(ns aoc2025.day03.solution
   (:require
    [clojure.string :as str]))
 
@@ -32,3 +32,40 @@
 
 (solve-1 sample-input)
 (solve-1 (slurp "resources/day03/input.txt"))
+
+
+(defn find-largest-n-digit-number [input n]
+  (let [digits (str/split input #"")
+        len (count digits)
+        memo (atom {})]
+    (if (> n len)
+      0
+      (letfn [(dp [idx remaining]
+                (if-let [cached (@memo [idx remaining])]
+                  cached
+                  (let [result (cond
+                                 (zero? remaining) ""
+                                 (> remaining (- len idx)) ""
+                                 (= idx len) ""
+                                 :else (let [take (str (digits idx) (dp (inc idx) (dec remaining)))
+                                             skip (dp (inc idx) remaining)
+                                             take-num (if (empty? take) 0 (parse-long take))
+                                             skip-num (if (empty? skip) 0 (parse-long skip))]
+                                         (if (>= take-num skip-num) take skip)))]
+                    (swap! memo assoc [idx remaining] result)
+                    result)))]
+        (parse-long (dp 0 n))))))
+
+(find-largest-n-digit-number "987654321111111" 12)
+(find-largest-n-digit-number "811111111111119" 12)
+(find-largest-n-digit-number "234234234234278" 12)
+(find-largest-n-digit-number "818181911112111" 12)
+(find-largest-n-digit-number "2213322222223222132231233322432224423222226232323522232215252332221122222231232224232722131522422232" 12)
+
+(defn solve-2 [input]
+  (->> (str/split-lines input)
+       (map #(find-largest-n-digit-number % 12))
+       (reduce +)))
+
+(solve-2 sample-input)
+(solve-2 (slurp "resources/day03/input.txt"))
