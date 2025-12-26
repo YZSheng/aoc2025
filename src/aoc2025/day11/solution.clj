9890034f4d7d924c5fe3d2b797a9580b8d0c711c
Sheng Yunzhou <shengyunzhou@gmail.com>
2025-12-26 09:51:46 +0800

    add day11 part 2

--- src/aoc2025/day11/solution.clj
+++ src/aoc2025/day11/solution.clj
@@ -1,4 +1,4 @@
-(ns aoc2025.day11.solution 
+(ns aoc2025.day11.solution
   (:require
    [clojure.string :as str]))
 
@@ -40,3 +40,45 @@ iii: out")
 (solve-1 sample-input)
 
 (solve-1 (slurp "resources/day11/input.txt"))
+
+
+(def sample-input-2 "svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out")
+
+
+(defn count-paths-through
+  ([graph start end must-visit]
+   (count-paths-through graph start end must-visit #{} (atom {})))
+  ([graph start end must-visit visited memo]
+   (let [memo-key [start end must-visit]]
+     (if (contains? @memo memo-key)
+       (@memo memo-key)
+       (let [result (if (= start end)
+                      (if (empty? must-visit) 1 0)
+                      (if (visited start)
+                        0
+                        (let [new-visited (conj visited start)
+                              new-must-visit (disj must-visit start)
+                              neighbors (get graph start [])]
+                          (reduce + 0 (for [neighbor neighbors]
+                                        (count-paths-through graph neighbor end new-must-visit new-visited memo))))))]
+         (swap! memo assoc memo-key result)
+         result)))))
+
+(defn solve-2 [input]
+  (let [graph (parse-input input)]
+    (count-paths-through graph "svr" "out" #{"fft" "dac"})))
+
+(solve-2 sample-input-2)
+(solve-2 (slurp "resources/day11/input.txt"))
